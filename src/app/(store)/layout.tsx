6416f57f2c5697a1f9c5a93a4bf6b8b1e3f7cf46
jdtheefirst <jngatia045@gmail.com>
2026-01-19 12:52:01 +0300

    remove global css from store layout

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,9 +1,5 @@
 // app/(store)/layout.tsx
 import { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
